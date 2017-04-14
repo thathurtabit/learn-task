bcccbfc1a8f30a0bfd49d305a856e940e886a6f0
Stephen Fairbanks <stephen@ghosthorses.co.uk>
2017-04-14 22:00:07 +0100

    Continuing updates

--- index.php
+++ index.php
@@ -60,8 +60,10 @@
             
             <div class="row">
               <div class="card-deck">
-                <div class="card">
-                  <img class="card-img-top" src="http://placehold.it/250x150" alt="Card image cap">
+                <a href="<?php echo $root ?>/category" class="card">
+                  <div class="card-img-top">
+                    <i class="fa fa-code"></i>
+                  </div>
                   <div class="card-block">
                     <h4 class="card-title">Developer Docs</h4>
                     <p class="card-text">How to get data in and out of Customer.io</p>
@@ -69,9 +71,11 @@
                   <div class="card-footer">
                     <small class="text-muted">Last updated 3 mins ago</small>
                   </div>
-                </div>
-                <div class="card">
-                  <img class="card-img-top" src="http://placehold.it/250x150" alt="Card image cap">
+                </a>
+                <a href="<?php echo $root ?>/category" class="card">
+                  <div class="card-img-top">
+                    <i class="fa fa-code"></i>
+                  </div>
                   <div class="card-block">
                     <h4 class="card-title">Getting Started</h4>
                     <p class="card-text">New to Customer.io? Start here!</p>
@@ -79,9 +83,11 @@
                   <div class="card-footer">
                     <small class="text-muted">Last updated 3 mins ago</small>
                   </div>
-                </div>
-                <div class="card">
-                  <img class="card-img-top" src="http://placehold.it/250x150" alt="Card image cap">
+                </a>
+                <a href="<?php echo $root ?>/category" class="card">
+                  <div class="card-img-top">
+                    <i class="fa fa-code"></i>
+                  </div>
                   <div class="card-block">
                     <h4 class="card-title">Help Docs</h4>
                     <p class="card-text">The nuts &amp; bolts of using Customer.io</p>
@@ -89,11 +95,13 @@
                   <div class="card-footer">
                     <small class="text-muted">Last updated 3 mins ago</small>
                   </div>
-                </div>
+                </a>
               </div>
               <div class="card-deck">
-                <div class="card">
-                  <img class="card-img-top" src="http://placehold.it/250x150" alt="Card image cap">
+                <a href="<?php echo $root ?>/category" class="card">
+                  <div class="card-img-top">
+                    <i class="fa fa-code"></i>
+                  </div>
                   <div class="card-block">
                     <h4 class="card-title">Primers</h4>
                     <p class="card-text">Resources to improve your campaigns</p>
@@ -101,9 +109,11 @@
                   <div class="card-footer">
                     <small class="text-muted">Last updated 3 mins ago</small>
                   </div>
-                </div>
-                <div class="card">
-                  <img class="card-img-top" src="http://placehold.it/250x150" alt="Card image cap">
+                </a>
+                <a href="<?php echo $root ?>/category" class="card">
+                  <div class="card-img-top">
+                    <i class="fa fa-code"></i>
+                  </div>
                   <div class="card-block">
                     <h4 class="card-title">REST API</h4>
                     <p class="card-text">API Documentation</p>
@@ -111,9 +121,11 @@
                   <div class="card-footer">
                     <small class="text-muted">Last updated 3 mins ago</small>
                   </div>
-                </div>
-                <div class="card">
-                  <img class="card-img-top" src="http://placehold.it/250x150" alt="Card image cap">
+                </a>
+                <a href="<?php echo $root ?>/category" class="card">
+                  <div class="card-img-top">
+                    <i class="fa fa-code"></i>
+                  </div>
                   <div class="card-block">
                     <h4 class="card-title">Recipes</h4>
                     <p class="card-text">Campaigns and projects, step by step</p>
@@ -121,42 +133,47 @@
                   <div class="card-footer">
                     <small class="text-muted">Last updated 3 mins ago</small>
                   </div>
-                </div>
+                </a>
               </div>
             </div>
+            
+
+            <div class="container mt-3">
+              <div class="row">
+                
+                <ul class="list-unstyled">
+                  <li class="media">
+                    <img class="d-flex mr-3" src="http://placehold.it/80x80" alt="Generic placeholder image">
+                    <div class="media-body">
+                      <h5 class="mt-0 mb-1">List-based media object</h5>
+                      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+                  </li>
+                  <li class="media my-3">
+                    <img class="d-flex mr-3" src="http://placehold.it/80x80" alt="Generic placeholder image">
+                    <div class="media-body">
+                      <h5 class="mt-0 mb-1">List-based media object</h5>
+                      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+                  </li>
+                  <li class="media">
+                    <img class="d-flex mr-3" src="http://placehold.it/80x80" alt="Generic placeholder image">
+                    <div class="media-body">
+                      <h5 class="mt-0 mb-1">List-based media object</h5>
+                      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+                  </li>
+                </ul>
+
+              </div>
+            </div>
+
+
           </div>
 
           <!-- / MAIN CONTENT -->
 
-          <div class="container mt-3">
-            <div class="row">
-              
-              <ul class="list-unstyled">
-                <li class="media">
-                  <img class="d-flex mr-3" src="http://placehold.it/80x80" alt="Generic placeholder image">
-                  <div class="media-body">
-                    <h5 class="mt-0 mb-1">List-based media object</h5>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                  </div>
-                </li>
-                <li class="media my-3">
-                  <img class="d-flex mr-3" src="http://placehold.it/80x80" alt="Generic placeholder image">
-                  <div class="media-body">
-                    <h5 class="mt-0 mb-1">List-based media object</h5>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                  </div>
-                </li>
-                <li class="media">
-                  <img class="d-flex mr-3" src="http://placehold.it/80x80" alt="Generic placeholder image">
-                  <div class="media-body">
-                    <h5 class="mt-0 mb-1">List-based media object</h5>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                  </div>
-                </li>
-              </ul>
-
-            </div>
-          </div>
+          
                                                 
           
         </div><!-- /.content -->
